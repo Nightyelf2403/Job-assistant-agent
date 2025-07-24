d8ac843a9bd626b2820b7684eceebfc0f1c02ced
Nightyelf2403 <chun8135@vandals.uidaho.edu>
2025-07-24 10:21:28 +0530

    Fix: use environment variable for API base URL

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
