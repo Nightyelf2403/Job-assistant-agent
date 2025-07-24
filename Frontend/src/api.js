ec5b6774141a96fe42ccf6ab59cb0c055ad2bdb8
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-07-24 11:33:53 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "http://localhost:5050/api"
   headers: {
     "Content-Type": "application/json",
   },
